04bd662f29e2598bb789494659e3981f1cf6d883
ashish23072002 <ashish23bisht@gmail.com>
2025-07-21 21:41:06 +0530

    Build a Full-Stack React App with Axios : CURD Operations

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-import './App.css'
-import Movie from "./Pages/Movie";
+import { useEffect } from "react";
+import { getPost } from "./api/PostApi";
+import "./App.css";
+// import Movie from "./Pages/Movie";
 
 const App = () => {
-  return <Movie />;
+  const getPostData = async () => {
+    try {
+      const res = await getPost();
+      console.log(res.data);
+      // setData(res.data.Search);
+    } catch (error) {
+      console.log("Erroe message", error.message);
+      console.log("Erroe Status", error.Status);
+      console.log("Erroe data", error.data);
+    }
+  };
+  useEffect(() => {
+    console.log(getPostData());
+  }, []);
+  return (
+    <h1>hello from post</h1>
+    // <Movie />
+  );
 };
 
 export default App;
